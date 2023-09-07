5cd0648ad0fa997b02e110830227136a53ff07bf
LuisilloSavagge <144190931+LuisilloSavagge@users.noreply.github.com>
2023-09-06 19:47:45 -0600

    Add files via upload

---
+++ p3_1s22.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+  Class p3_1s22{
+ 	 public static void main(String[] args) {
+ 	 String nombre;
+ 	 Scanner teclado= new Scanner(System.in);
+ 	 System.out.Println("ingresa tu nombre: ");
+ 	 nombre = teclado .Nextline	
+ 	 }
+ }
